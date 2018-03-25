1ce4030345fef31afd6f8222969744139068f98d
zhuzl <992537958@qq.com>
2018-03-26 00:16:53 +0800

    add base file

---
+++ threadPool/threadpool.cpp
@@ -0,0 +1,6 @@
+#include "threadpool.h"
+
+threadPool::threadPool()
+{
+
+}
---
+++ threadPool/threadpool.h
@@ -0,0 +1,11 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+
+class threadPool
+{
+public:
+    threadPool();
+};
+
+#endif // THREADPOOL_H
